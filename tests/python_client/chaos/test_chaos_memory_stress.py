a12eebada6fdca832bc75d6cf855b5ac3dcdd599
ThreadDao <yufen.zong@zilliz.com>
2022-01-25 11:20:30 +0800

    [skip e2e] Test chaos querynode memory stress
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

--- tests/python_client/chaos/test_chaos_memory_stress.py
+++ tests/python_client/chaos/test_chaos_memory_stress.py
@@ -52,20 +52,22 @@ class TestChaosData:
         nb = 100000
         per_nb = 50000
         dim = 512
-        c_name = cf.gen_unique_str('chaos_memory')
+        # c_name = cf.gen_unique_str('chaos_memory')
+        c_name = 'chaos_memory_mVr2HHwn'
         collection_w = ApiCollectionWrapper()
         collection_w.init_collection(name=c_name,
                                      schema=cf.gen_default_collection_schema(dim=dim))
-        for i in range(nb // per_nb):
-            t0 = datetime.datetime.now()
-            df = cf.gen_default_dataframe_data(nb=per_nb, dim=dim)
-            res = collection_w.insert(df)[0]
-            assert res.insert_count == per_nb
-            log.info(f'After {i + 1} insert, num_entities: {collection_w.num_entities}')
-            tt = datetime.datetime.now() - t0
-            log.info(f"{i} insert and flush data cost: {tt}")
+        # for i in range(nb // per_nb):
+        #     t0 = datetime.datetime.now()
+        #     df = cf.gen_default_dataframe_data(nb=per_nb, dim=dim)
+        #     res = collection_w.insert(df)[0]
+        #     assert res.insert_count == per_nb
+        #     log.info(f'After {i + 1} insert, num_entities: {collection_w.num_entities}')
+        #     tt = datetime.datetime.now() - t0
+        #     log.info(f"{i} insert and flush data cost: {tt}")
 
         # apply memory stress chaos
+        log.info(collection_w.num_entities)
         chaos_config = gen_experiment_config(chaos_yaml)
         log.debug(chaos_config)
         chaos_res = CusResource(kind=chaos_config['kind'],
@@ -91,7 +93,7 @@ class TestChaosData:
                 query_res, _ = collection_w.query(term_expr)
                 assert len(query_res) == 1
 
-                search_res, _ = collection_w.search(cf.gen_vectors(1, ct.default_dim),
+                search_res, _ = collection_w.search(cf.gen_vectors(1, dim),
                                                     ct.default_float_vec_field_name,
                                                     ct.default_search_params, ct.default_limit)
                 log.debug(search_res[0].ids)
