ca83e546ae3c3dee2794f74cb9c0bc026805f338
ThreadDao <yufen.zong@zilliz.com>
2022-06-27 17:57:12 +0800

    [test][skip-e2e] Update scale test image tag to 2.1 and update operator apiVersion
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

--- tests/python_client/customize/milvus_operator.py
+++ tests/python_client/customize/milvus_operator.py
@@ -7,10 +7,12 @@ from common.cus_resource_opts import CustomResourceOperations as CusResource
 
 template_yaml = os.path.join(os.path.dirname(__file__), 'template/default.yaml')
 MILVUS_GRP = 'milvus.io'
-MILVUS_VER = 'v1alpha1'
-MILVUS_PLURAL = 'milvusclusters'
-MILVUS_KIND = 'MilvusCluster'
-
+# MILVUS_VER = 'v1alpha1'
+MILVUS_VER = 'v1beta1'
+# MILVUS_PLURAL = 'milvusclusters'
+MILVUS_PLURAL = 'milvuses'
+# MILVUS_KIND = 'MilvusCluster'
+MILVUS_KIND = 'Milvus'
 
 class MilvusOperator(object):
     def __init__(self):
@@ -149,3 +151,8 @@ class MilvusOperator(object):
             endpoint = res_object['status']['endpoint']
 
         return endpoint
+
+if __name__ == '__main__':
+    mic = MilvusOperator()
+    release = 'expand-index'
+    mic.wait_for_healthy(release, 'chaos-testing')
