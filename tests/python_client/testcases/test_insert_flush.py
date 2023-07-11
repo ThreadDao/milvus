4a7cc0085c4c8e33444edc7017b04a5422316dc5
ThreadDao <yufen.zong@zilliz.com>
2023-07-11 16:42:28 +0800

    Test insert flush loop
    Signed-off-by: ThreadDao <yufen.zong@zilliz.com>

---
+++ tests/python_client/testcases/test_insert_flush.py
@@ -0,0 +1,58 @@
+import pandas as pd
+
+from base.client_base import TestcaseBase
+from pymilvus import connections
+from base.collection_wrapper import ApiCollectionWrapper
+from base.schema_wrapper import ApiCollectionSchemaWrapper
+from utils.util_log import test_log as log
+from common import common_func as cf
+from common import common_type as ct
+
+
+class TestInsertFlush:
+    """ Test case of Insert interface """
+
+    def test_insert_flush(self):
+        """
+        target: test insert DataFrame data
+        method: 1.create collection
+                2.insert dataframe data
+        expected: assert num entities
+        """
+        nb = 1000000
+        ni = 10
+        dim = 128
+
+        #  connect
+        connections.connect(host=cf.param_info.param_host, port=cf.param_info.param_port)
+
+        # create collection with 1 shard and auto id
+
+        c_name = cf.gen_unique_str("insert_flush")
+        fields = [cf.gen_int64_field(is_primary=True), cf.gen_float_vec_field(dim=dim)]
+        schema, _ = ApiCollectionSchemaWrapper().init_collection_schema(fields=fields, description=ct.default_desc,
+                                                                        primary_field=ct.default_int64_field_name,
+                                                                        auto_id=True)
+        collection_w = ApiCollectionWrapper()
+        collection_w.init_collection(name=c_name, schema=schema, shards_num=1)
+
+        # create index
+        index_params = {"index_type": "IVF_SQ8", "metric_type": "L2", "params": {"nlist": 64}}
+        collection_w.create_index(ct.default_float_vec_field_name, index_params, index_name=ct.default_index_name)
+
+        # load
+        collection_w.load()
+
+        # insert for loop
+        ni_count = nb // ni
+        for i in range(ni_count):
+            float_vec_values = cf.gen_vectors(ni, dim)
+            df = pd.DataFrame({
+                ct.default_float_vec_field_name: float_vec_values
+            })
+            mutation_res, _ = collection_w.insert(data=df)
+            assert mutation_res.insert_count == ni
+            # flush
+            assert collection_w.num_entities == ni*(i+1)
+
+        log.info(f'Insert collection {collection_w.name} success with entities {collection_w.num_entities}')
